4764f39affafe869bf7d72a5c6bd99771a3c5090
Muhammad Mas3oud <mebramasoud@gmail.com>
2025-05-16 15:22:13 +0300

    full name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="hero-section" id="home">
       <div className="hero-content-wrapper">
         <Fragment>
-          <h1 className="hero-title">Muhammad Masoud</h1>
+          <h1 className="hero-title">Muhammad Ebrahim Masoud</h1>
           <h2 className="hero-subtitle">Web Developer & Designer</h2>
           <a href="#footer" className="hero-contact-button">CONTACT ME</a>
         </Fragment>
